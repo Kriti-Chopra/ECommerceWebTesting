053b475ae553c0a79f9d6a3b9c26bb317c1d95c0
Kriti-Chopra <37541206+Kriti-Chopra@users.noreply.github.com>
2025-02-09 21:47:48 +0530

    Page definitions for login and products dashboard

--- src/PageObjects/LoginPage.java
+++ src/PageObjects/LoginPage.java
@@ -1,24 +1,56 @@
 package PageObjects;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 	
+	//Anshika@gmail.com
+	//Iamking@000
+	
+	WebDriver driver;
+	public LoginPage() {
+		this.driver=Browser.driver;
+	}
 	
 	//Region :page elements
 	
-	WebElement emailInputBox = Browser.driver.findElement(By.id("userEmail"));
-	WebElement passwordInputBox = Browser.driver.findElement(By.id("userPassword"));
-	WebElement loginBtn = Browser.driver.findElement(By.id("login"));
+	WebElement emailInputBox = driver.findElement(By.id("userEmail"));
+	WebElement passwordInputBox = driver.findElement(By.id("userPassword"));
+	WebElement loginBtn = driver.findElement(By.id("login"));
+	WebElement forgotPasswordLnk = driver.findElement(By.cssSelector(".forgot-password-link"));
+	WebElement registerLnk = driver.findElement(By.cssSelector(".text-reset"));
 	
 	//Region : page methods
-	
-	//logs in with the provided email and password
-	public void Login(String email, String password) {
+
+	//enter email string
+	public void EnterEmail(String email) {
 		emailInputBox.sendKeys(email);
+	}
+	
+	//enter password string
+	public void EnterPassword(String password) {
 		passwordInputBox.sendKeys(password);
+	}
+	
+	//Click Login Btn
+	public void ClickLogin() {
 		loginBtn.click();
 	}
 	
+	//logs in with the provided email and password
+	public void Login(String email, String password) {
+		EnterEmail(email);
+		EnterPassword(password);
+		ClickLogin();
+	}
+	
+	public void ClickForgotPassword() {
+		forgotPasswordLnk.click();
+	}
+	
+	public void ClickRegister() {
+		registerLnk.click();
+	}
 }
--- src/PageObjects/ProductsDashboard.java
+++ src/PageObjects/ProductsDashboard.java
@@ -13,16 +13,36 @@ public class ProductsDashboard {
 		this.driver=Browser.driver;
 	}
 
+	//region page elements
 	List<WebElement> productsList = driver.findElements(By.cssSelector(".card"));
 	WebElement cartBtn = driver.findElement(By.xpath("//button[contains(@routerlink,'cart')]"));
 	
+	//select a product by product name
 	public void AddProductToCart(String productName) {
 		WebElement product =
 		productsList.stream().filter(p->p.findElement(By.tagName("b")).getText().equals(productName)).findFirst().orElse(null);
 		product.findElement(By.cssSelector(".card-body button:last-of-type")).click();
 	}
 	
+	//select multiple products
+	public void AddProductToCart(List<String> products) {	
+		for(String productName:products) {
+			WebElement product =
+					productsList.stream().filter(p->p.findElement(By.tagName("b")).getText().equals(productName)).findFirst().orElse(null);
+			product.findElement(By.cssSelector(".card-body button:last-of-type")).click();	
+		}
+	}
+	
+	//clicks cart button
 	public void GoToCart() {
 		cartBtn.click();
 	}
+	
+	//get count of products added to cart
+	public int GetCartCount() {
+		WebElement countLabel = cartBtn.findElement(By.tagName("label"));
+		return Integer.parseInt(countLabel.getText());
+	}
+	
+	
 }
