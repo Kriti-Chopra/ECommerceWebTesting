ee874b693d10993765662676db6f0e75ec66be55
Kriti-Chopra <37541206+Kriti-Chopra@users.noreply.github.com>
2025-02-13 23:20:16 +0530

    payment types

--- src/PageObjects/OrderDashboard.java
+++ src/PageObjects/OrderDashboard.java
@@ -1,5 +1,37 @@
 package PageObjects;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class OrderDashboard {
 
+	WebDriver driver;
+	public void OrderDashboard() {
+		this.driver=Browser.driver;
+	}
+	
+	//contains main div as first element
+	List<WebElement> listOfPaymentTypes = driver.findElements(By.xpath("//div[contains(@class,'payment__type')]"));
+	
+	
+	public List<String> GetAvailablePaymentTypes() {
+		List<String> paymentTypesList = new ArrayList<String>();
+		for(int i=1;i<listOfPaymentTypes.size();++i) {
+			paymentTypesList.add(listOfPaymentTypes.get(i).getText());
+		}
+		return paymentTypesList;
+	}
+	
+	public void SelectPaymentType(String paymentType) {
+		for(int i=1;i<listOfPaymentTypes.size();++i) {
+			if(listOfPaymentTypes.get(i).getText().equals(paymentType)) {
+				listOfPaymentTypes.get(i).click();
+				return;
+			}
+		}
+	}
 }
