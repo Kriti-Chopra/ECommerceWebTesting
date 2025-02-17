64bc78cc860743ae1fd47d681a0b9f50840895fb
Kriti-Chopra <37541206+Kriti-Chopra@users.noreply.github.com>
2025-02-17 22:41:22 +0530

    customer details + typeahead

--- src/PageObjects/OrderDashboard.java
+++ src/PageObjects/OrderDashboard.java
@@ -17,6 +17,10 @@ public class OrderDashboard {
 	//contains main div as first element
 	List<WebElement> listOfPaymentTypes = driver.findElements(By.xpath("//div[contains(@class,'payment__type')]"));
 	
+	//shipping info area
+	WebElement usernameInput = driver.findElement(By.xpath("(//div[contains(@class,'user__name')]//input)[1]"));
+	WebElement countryAutopopulateInput = driver.findElement(By.xpath("//div[contains(@class,'user__address')]//input"));
+	List<WebElement> populatedCountryList = driver.findElements(By.xpath("//section[contains(@class,'ta-results')]/button/span"));
 	
 	public List<String> GetAvailablePaymentTypes() {
 		List<String> paymentTypesList = new ArrayList<String>();
@@ -34,4 +38,18 @@ public class OrderDashboard {
 			}
 		}
 	}
+	
+	public void SelectCountry(String countryName) {
+		countryAutopopulateInput.sendKeys(countryName);
+		
+		int i=0;
+		while(populatedCountryList.size()>0) {
+			if(populatedCountryList.get(i).getText().equals(countryName)) {
+				populatedCountryList.get(i).click();
+				break;
+			}
+			++i;
+		}	
+	}
+	
 }
